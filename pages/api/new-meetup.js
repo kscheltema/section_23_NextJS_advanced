4bedd7d2c5c6516a877d229e20fb18384e0c8457
kscheltema <k87scheltema@outlook.com>
2021-07-10 16:18:55 +0200

    fetch and use data from MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -6,9 +6,6 @@ async function handler(req, res) {
   const data = req.body;
   
   const client = await MongoClient.connect('mongodb+srv://Seth:Twilight26066701@cluster0.epecw.mongodb.net/meetups?retryWrites=true&w=majority')
-  //this link comes from the mongo connect modal
-  //mongodb+srv://<username>:<password>@cluster0.epecw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-  //replace the <username> and <password> with actual life users connected to the DB
 
   const db = client.db();
   const meetupsCollection = db.collection('meetups');
