53188c65248bcd649d80f32d77f385951a885158
kscheltema <k87scheltema@outlook.com>
2021-07-10 16:20:14 +0200

    pre-rndering data from a DB not hardcoded

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9b77950674e13ad15860";
+/******/ 	var hotCurrentHash = "1e3de59cbae45165630d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,6 @@ import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
 import Layout from '../components/layout/Layout';
 
-const DUMMY_MEETUPS = [
-  {
-id: 'm1',
-title: 'A First Meetup',
-address: 'Some address 5, some city, 123456',
-image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-description: 'this is a first meetup',
-},
-  {
-id: 'm2',
-title: 'A Second Meetup',
-address: 'Some address 6, some city, 123456',
-image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-description: 'this is a second meetup',
-}
-];
-
 function HomePage(props) {
 
   return <Layout>
